82a625d9198bed962a606fb5eff41e3eb742cba4
Marshyswallow <v3331587@gmail.com>
2026-07-07 17:11:32 +0530

    Improve cookie security settings in authController
    Updated cookie settings to enhance security by enabling secure flag and changing sameSite policy.

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -87,8 +87,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
@@ -110,8 +110,8 @@ export const logOut = async (req, res) => {
 
     res.clearCookie("token", {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
     });
 
     return res.status(200).json({
@@ -237,8 +237,8 @@ export const googleAuth = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
