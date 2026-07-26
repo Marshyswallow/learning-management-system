4b5fb125e11da0e46661c2bed6881468bfe5eb08
Marshyswallow <v3331587@gmail.com>
2026-07-26 14:15:33 +0530

    corrected cors

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,52 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDb from './config/connectDB.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/connectDB.js";
 import cookieParser from "cookie-parser";
-import authRouter from './route/authRoute.js';
-import cors from "cors"
-import userRouter from './route/userRoute.js';
-import courseRouter from './route/courseRoute.js';
+import authRouter from "./route/authRoute.js";
+import cors from "cors";
+import userRouter from "./route/userRoute.js";
+import courseRouter from "./route/courseRoute.js";
 import lectureRouter from "./route/lectureRoute.js";
 import orderRouter from "./route/orderRoute.js";
-
-
 
 dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-  origin:"*",
-  credentials:true
-}))
+app.use(express.json());
+app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "hhttps://learning-management-system-j4tz.vercel.app",
+];
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))  // ✅ add this
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
 
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/course",courseRouter)
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // ✅ add this
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/course", courseRouter);
 app.use("/api/lecture", lectureRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("hello from server");
 });
-
-
 
 app.listen(port, () => {
   console.log("server started");
